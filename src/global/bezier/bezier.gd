f52a03122c16f64fa206c463fd608a0ac5af5475
EdGeber <egma@cin.ufpe.br>
2022-05-04 03:30:03 -0300

    Encapsulate and optimize Bézier computations

---
+++ src/global/bezier/bezier.gd
@@ -0,0 +1,100 @@
+class_name Bezier
+
+const epsilon: = 1e-1
+
+var control_points: PoolVector2Array
+
+var cached_pabs: = PoolVector2Array()  # a 'pabs' is a pair (t, x) of a parametric curve
+var cache_size: = 50
+var last_closest_pabs_index: = 0  # last left closest point index
+
+
+func _init(
+	radius_control_points: PoolVector2Array,
+	cache_size: = self.cache_size):
+	
+	self.control_points = radius_control_points
+	self.cache_size = cache_size
+	cachePabs()
+	return self
+
+func equalsMinusZero(x: float) -> bool:
+	return -epsilon <= x and x < 0
+
+func linear_interpolate(A, B, t: float):
+	return (1-t)*A + t*B
+
+func curveAbscissa(t: float) -> float:
+	# uses the control points from radius_control_points
+	var mid_points: = control_points
+	
+	var n: = mid_points.size() - 1
+	while n > 0:
+		for i in range(n):
+			var a: float = mid_points[i].x
+			var b: float = mid_points[i+1].x
+			mid_points[i].x = linear_interpolate(a, b, t)
+		n -= 1
+	return mid_points[0].x
+
+func curve(t: float) -> Vector2:
+	# uses the control points from radius_control_points
+	var mid_points: PoolVector2Array = control_points
+	var n: = mid_points.size() - 1
+	while n > 0:
+		for i in range(n):
+			var A = mid_points[i]
+			var B = mid_points[i+1]
+			mid_points[i] = linear_interpolate(A, B, t)
+		n -= 1
+	return mid_points[0]
+
+func cachePabs():
+	var t: = 0.0
+	var increment: = 1.0/(cache_size-1)
+	while t < 1.0:
+		cached_pabs.append(Vector2(t, curveAbscissa(t)))
+		t += increment
+	var final_control_point: = control_points[control_points.size()-1]
+	cached_pabs.append(Vector2(1, final_control_point.x))
+
+func findLeftClosestCachedPabsIndex(x: float) -> int:
+	var start: = last_closest_pabs_index
+	var closest_pabs_index: = start
+	var closest_pabs: = cached_pabs[closest_pabs_index]
+	
+	var curr_pabs: Vector2
+	
+	var closest_dist: = abs(x - closest_pabs[1])
+	var curr_dist: float
+	
+	# +1 because checking start would be redundant
+	# -1 so that the chosen pabs is not the last one
+	for i in range(start+1, cache_size-1):
+		curr_pabs = cached_pabs[i]
+		curr_dist = abs(x - curr_pabs[1])
+		if curr_dist > closest_dist: break
+		closest_pabs_index = i
+		closest_dist = curr_dist
+		closest_pabs = curr_pabs
+	
+	if x < closest_pabs[1]:
+		return closest_pabs_index-1
+	return closest_pabs_index
+
+func function(x: float) -> float:
+	var tolerance: = 1e-4
+	var max_iterations = 100
+	
+	var i: = findLeftClosestCachedPabsIndex(x)
+	print(cached_pabs.size())
+	var tMin: = cached_pabs[i][0]
+	var tMax: = cached_pabs[i+1][0]
+	var tMid: = (tMin + tMax)/2.0
+	var found: Vector2 = curve(tMid)
+	while not(found.x - tolerance <= x and x <= found.x + tolerance):
+		if found.x < x: tMin = tMid
+		else: tMax = tMid
+		tMid = (tMin + tMax)/2.0
+		found = curve(tMid)
+	return found.y
--- src/global/photism/photism.gd
+++ src/global/photism/photism.gd
@@ -1,23 +1,15 @@
 extends Node2D
 
-const epsilon: = 1e-1
-
 var draw_rel_pos: = Vector2(0.5, 0.5)
 var lifetime: = 0.0
 var curr_radius: = 0.0
 # the values taken by the curve are assumed to be between 0 and 1
-export var duration: = 5.0
-export var radius_control_points: = PoolVector2Array()
+export(PoolVector2Array) var radius_control_points: = PoolVector2Array()
 export(float, 1, 100) var scaler: = 50.0
 
+var bezier: Bezier
+
 var initialized: = false
-
-
-func equalsMinusZero(x: float) -> bool:
-	return -epsilon <= x and x < 0
-
-func linear_interpolate(A: Vector2, B: Vector2, t: float) -> Vector2:
-	return (1-t)*A + t*B
 
 func quadratic(t: float) -> float:
 	# quadradic radius function
@@ -27,51 +19,24 @@ func quadratic(t: float) -> float:
 	var r = max_radius*(1 - pow(2*t/duration - 1, 2)) 
 	return r
 
-func bezierCurve(t: float) -> Vector2:
-	# uses the control points from radius_control_points
-	var mid_points: PoolVector2Array = radius_control_points
-	var n: = mid_points.size() - 1
-	while n > 0:
-		for i in range(n):
-			var A = mid_points[i]
-			var B = mid_points[i+1]
-			mid_points[i] = linear_interpolate(A, B, t)
-		n -= 1
-	return mid_points[0]
-
-func bezierFunc(x: float) -> float:
-	var tMin: = 0.0
-	var tMax: = 1.0
-	var tMid: = 0.5
-	var tolerance: = 1e-2
-	var found: Vector2 = bezierCurve(tMid)
-	while not(found.x <= x and x <= found.x + tolerance):
-		if found.x < x: tMin = tMid
-		else: tMax = tMid
-		tMid = (tMin + tMax)/2
-		found = bezierCurve(tMid)
-	return found.y
-
 func initialize(
 	draw_rel_pos: = self.draw_rel_pos,
 	radius_control_points: = self.radius_control_points,
 	scaler: float = self.scaler):
 	
 	self.draw_rel_pos = draw_rel_pos
-	self.radius_control_points = radius_control_points
 	self.scaler = scaler
+	self.bezier = Bezier.new(radius_control_points)
 	initialized = true
 	return self
 
 func radius(time: float) -> float:
-	return scaler*bezierFunc(time)
+	return scaler*bezier.function(time)
 
-
-func _enter_tree():
-	if not initialized:
-		initialize()
 
 func _ready():
+	if not initialized:
+		initialize()
 	set_physics_process(false)
 	yield(get_tree().create_timer(2.0), "timeout")
 	set_physics_process(true)
@@ -83,7 +48,6 @@ func _draw():
 func _physics_process(delta):
 	lifetime += delta
 	curr_radius = radius(lifetime)
-	print(lifetime)
 	
 	if curr_radius < 0:
 		call_deferred("queue_free")
